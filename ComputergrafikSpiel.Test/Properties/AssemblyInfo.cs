7270a86a4848bd9e6af5078b98a8643e114c25ba
Waldemar <wowa.lehner@gmail.com>
2020-04-07 17:24:19 +0200

    Add empty code project
    Includes Code Project and Testing Project. Testing Project references Code Project.

---
+++ ComputergrafikSpiel.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComputergrafikSpiel.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComputergrafikSpiel.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60e3d698-ea22-4a21-9c37-05f3ea37ffe1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ComputergrafikSpiel.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputergrafikSpiel.Test
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
