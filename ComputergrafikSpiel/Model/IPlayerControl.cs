a5a75ab280f328f3b663e0ebf8ea9f1466e0ce72
bautzn <nico.bautz@hs-weingarten.de>
2020-04-29 19:51:44 +0200

    Added IPlayerControl

---
+++ ComputergrafikSpiel/Model/IPlayerControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ComputergrafikSpiel.Model
+{
+    internal interface IPlayerControl
+    {
+        // Receives a enum list of pressed player actions -> Up, Down, Left, Right, Dash, Attack
+        void PlayerControl(IReadOnlyList<PlayerActionEnum.PlayerActions> actions);
+    }
+}
