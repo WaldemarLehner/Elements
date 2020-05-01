f1b7851c82500257d0768338ce4ee56f1ac9d2c7
The  🅻🅴🅶🅴🅽🅳 <zuzumachen1@web.de>
2020-05-01 18:19:54 +0200

    Eingefügt, schauen ob es passt.

---
+++ ComputergrafikSpiel/Controller/Controller.cs
@@ -0,0 +1,56 @@
+﻿using OpenTK;
+using ComputergrafikSpiel.View;
+using ComputergrafikSpiel.Model;
+using System;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace ComputergrafikSpiel.Controller
+{
+    internal class Controller : GameWindow
+
+    {
+
+        private IView View { get; set; }
+
+        private IUpdateModel Model { get; set; }
+
+        private IModel IModel { get; set; }
+
+        private InputController InpController { get; set; }
+
+        internal Controller(IView view, IModel model, int width, int height, string title)
+            : base(width, height, GraphicsMode.Default, title)
+        {
+            this.View = view;
+            this.Model = model;
+        }
+
+        //OnResize soll den Trigger an View weiterleiten
+        protected override void OnResize(EventArgs e)
+        {
+            this.View.Resize(this.Width, this.Height);
+            base.OnResize(e);
+        }
+
+        //OnRenderFrame soll den Trigger an View weiterleiten
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            //Context.SwapBuffers();
+            this.View.Render();
+            base.OnRenderFrame(e);
+        }
+
+        //OnUpdateFrame soll den Trigger an Model und Input Controller weiterleiten
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            this.InpController.PlayerAction();
+            this.Model.Update((float)e.Time);   //fragen ob notwendig
+            base.OnUpdateFrame(e);
+        }
+
+
+
+    }
+
+}
