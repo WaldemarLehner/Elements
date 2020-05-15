9fed90e1a2fd8bf2b78a7db606517ecf73576ce6
The  🅻🅴🅶🅴🅽🅳 <zuzumachen1@web.de>
2020-05-15 15:59:23 +0200

    ICharacter erstellt in Model. Eventhandling, OnHit, OnDeath, OnMove

---
+++ ComputergrafikSpiel/Model/ICharacter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputergrafikSpiel.Model
+{
+    internal class ICharacter
+    {
+
+        private int MaxHealth;
+        //private int MovementSpeed; müssen noch rein
+        //private int AttackSpeed;
+
+        public ICharacter(int passedMaxHealth)
+        {
+            this.MaxHealth = passedMaxHealth;
+
+            if (this.MaxHealth == 0)
+            {
+                /*Falls wir Argumente mit einbinden müssen
+                 * CharacterEventArgs args = new CharacterEventArgs();
+                //args.TimeReached = DateTime.Now;*/
+                OnDeath(EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler PlayerDeath;
+
+        public event EventHandler PlayerHit;
+
+        public event EventHandler PlayerMove;
+
+        protected virtual void OnDeath(EventArgs e)
+        {
+            EventHandler handler = PlayerDeath;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnHit(EventArgs e)
+        {
+            EventHandler handler = PlayerHit;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnMove(EventArgs e)
+        {
+            EventHandler handler = PlayerMove;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+    }
+
+    /* Nur Falls wir Argumente brauchen (erstmal aber nicht)
+      public class CharacterEventArgs : EventArgs
+    {
+        public DateTime TimeReached { get; set; }
+    }*/
+}
