a0d1d2334e00224f6b39c26a89ad44594ff541db
Waldemar <wowa.lehner@gmail.com>
2020-05-15 18:38:25 +0200

    Add IPositionable, move Positional Properties from IRenderable to IPositionable, let IRenderable inherit from IPositionable

---
+++ ComputergrafikSpiel/Model/IPositionable.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK;
+
+namespace ComputergrafikSpiel.Model
+{
+    internal interface IPositionable
+    {
+        Vector2 Position { get; }
+
+        Vector2 Scale { get; }
+
+        float Rotation { get; }
+
+        Vector2 RotationAnker { get; }
+    }
+}
