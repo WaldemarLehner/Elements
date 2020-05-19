86d625d9918c1a8b1b12409fc33b084d8bb5986c
Gerald Lautenschlager <Zoruo@outlook.com>
2020-05-19 17:07:36 +0200

    Split IPositionable into itself, IRotatable and IScalable, all accessable through ITransformable

---
+++ ComputergrafikSpiel/Model/ITransformable.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputergrafikSpiel.Model
+{
+    internal interface ITransformable
+    {
+        IPositionable Position { get; }
+
+        IRotatable Rotate { get; }
+
+        IScalable Scale { get; }
+    }
+}
