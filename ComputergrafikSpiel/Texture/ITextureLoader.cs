e548a5b8db9eff27f693d8d983dea0b75f970ccf
Julian Lingnau <zuzumachen1@web.de>
2020-05-20 12:39:21 +0200

    Texture Modul liegt nun in Model/Texture. In ITextureLoader Funktion LoadTexture gibt nun ein ITexture zurück.

--- ComputergrafikSpiel/Texture/ITextureLoader.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ComputergrafikSpiel.Texture
-{
-    internal interface ITextureLoader
-    {
-        void LoadTexture(string pathOrIdentifier); // Noch prüfen welcher Rückgabewert verlangt
-    }
-}
