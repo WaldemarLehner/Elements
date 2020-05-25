df89fbb880686f972748a8e6484e4c07cfc188f2
bautzn <nico.bautz@hs-weingarten.de>
2020-05-25 13:09:13 +0200

    Added IUpdateable

---
+++ ComputergrafikSpiel/Model/Interfaces/IUpdateable.cs
@@ -0,0 +1,8 @@
+﻿namespace ComputergrafikSpiel.Model.Interfaces
+{
+    // dtime => time between 2 Frames in OnUpdateFrame
+    internal interface IUpdateable
+    {
+        void Update(float dtime);
+    }
+}
