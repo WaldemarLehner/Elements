4e738f489744e2b2d516329c91b73a6b34d7f68d
Julian Lingnau <zuzumachen1@web.de>
2020-06-08 15:31:55 +0200

    IWorldTileCollidable erbt von IWorldTile. Somit Variablen in IWorlTile definiert.

--- ComputergrafikSpiel/Model/World/Interfaces/IWorldTile.cs
+++ ComputergrafikSpiel/Model/World/Interfaces/IWorldTile.cs
@@ -5,5 +5,10 @@ namespace ComputergrafikSpiel.Model.World
 {
     public interface IWorldTile : IRenderable
     {
+        // Enum -> ColType wird erst noch definiert
+        // ColType CollidableType { get; }
+
+        // X, Y Koordinaten, wo Grid positioniert werden soll
+        (int x, int y) GridPosition { get; }
     }
 }
--- ComputergrafikSpiel/Model/World/Interfaces/IWorldTileCollidable.cs
+++ ComputergrafikSpiel/Model/World/Interfaces/IWorldTileCollidable.cs
@@ -4,12 +4,5 @@ namespace ComputergrafikSpiel.Model.World.Interfaces
 {
     public interface IWorldTileCollidable : IWorldTile, ICollidable
     {
-        // Enum -> ColType wird erst noch definiert
-        // ColType CollidableType { get; }
-
-        // X, Y Koordinaten, wo Grid positioniert werden soll
-        int PositionX { get; }
-
-        int PositionY { get; }
     }
 }
