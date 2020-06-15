fe417e575f59bddc6f769e205612512a35f3128d
Gerald Lautenschlager <Zoruo@outlook.com>
2020-06-15 12:34:41 +0200

    Implemented Trigger and ITrigger. Still has to be made usable with the ColliderManager

---
+++ ComputergrafikSpiel/Model/Trigger/Interfaces/ITrigger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputergrafikSpiel.Model.Collider.Interfaces;
+
+namespace ComputergrafikSpiel.Model.Trigger.Interfaces
+{
+    public interface ITrigger : ICollidable
+    {
+    }
+}
---
+++ ComputergrafikSpiel/Model/Trigger/Trigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputergrafikSpiel.Model.Collider;
+using ComputergrafikSpiel.Model.Collider.Interfaces;
+using ComputergrafikSpiel.Model.Trigger.Interfaces;
+using OpenTK;
+
+namespace ComputergrafikSpiel.Model.Trigger
+{
+    internal class Trigger : ITrigger
+    {
+        internal Trigger(IColliderManager colliderManager, Vector2 position)
+        {
+            // will have to be changed
+            this.Collider = new CircleOffsetCollider(this, Vector2.Zero, 32);
+            this.ColliderManager = colliderManager;
+            this.Position = position;
+        }
+
+        public IColliderManager ColliderManager { get; }
+
+        public ICollider Collider { get; }
+
+        public Vector2 Position { get; }
+
+        public float Rotation { get; } = 0f;
+
+        public Vector2 RotationAnker { get; } = Vector2.Zero;
+
+        // may have to be changed alongside the radius of the Collider
+        public Vector2 Scale { get; } = Vector2.One * 32;
+    }
+}
