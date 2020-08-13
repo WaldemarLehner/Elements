fbc2c5cab44e5151b5ce9bf603a1c2d0a2c14f47
bautzn <nantotv24@gmail.com>
2020-08-13 22:12:50 +0200

    Fixed Upgrade Screen

--- ComputergrafikSpiel/Model/Overlay/UpgradeScreen/UpgradeScreenButtonTextureLookupGenerator.cs
+++ ComputergrafikSpiel/Model/Overlay/UpgradeScreen/UpgradeScreenButtonTextureLookupGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using ComputergrafikSpiel.Model.Character.Player;
 using ComputergrafikSpiel.Model.EntitySettings.Texture;
@@ -20,10 +21,10 @@ namespace ComputergrafikSpiel.Model.Overlay.UpgradeScreen
         private static string GetUpgradeText(float valueBefore, float improvement, float valueAfter)
         {
             CultureInfo invC = CultureInfo.InvariantCulture;
-            return $"{valueBefore.ToString(invC)} > {valueAfter.ToString(invC)} ";
+            return $"{decimal.Round((decimal)valueBefore, 2).ToString(invC)} > {decimal.Round((decimal)valueAfter, 2).ToString(invC)} ";
         }
 
-        private static string Truncate(this float value) => string.Format("{0:0.00}", value);
+        // private static string Truncate(this float value) => string.Format("{0:0.00}", value).ToString(invC);
 
         private static string GetName(PlayerEnum.Stats s)
         {
